cfc1fa31931e7472e0ba53b79bd969ecacd09b61
Grodrigues22 <gmrodrigues98@gmail.com>
2022-10-07 19:00:21 -0300

    adicionado_string_1e2

---
+++ exemplos_das_aulas/05 -dominando_strinds_e_fatiamento/string_2.py
@@ -0,0 +1,26 @@
+from unicodedata import name
+
+
+nome = "Gabriel"
+idade = 24
+professao = "Programado"
+linguagem = "Python"
+saldo = 45.435
+
+dados = {"nome": "Gabriel", "idade": 24}
+
+# formas das menos ultilizadas a mais recente no python
+print("Nome: %s Idade: %s" % (nome, idade))
+
+print("Nome: {} Idade: {}".format(nome, idade))
+
+print("Nome: {1} Idade: {0}".format(idade, nome))
+print("Nome: {1} Idade: {0} Nome: {1} {1}".format(nome, idade))
+
+print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
+print("Nome: {name} Idade: {age} {name} {age}".format(age=nome, name=idade))
+print("Nome: {nome} Idade: {idade}".format(**dados))
+
+print(f"Nome: {nome} Idade: {idade}")
+print(f"Nome: {nome} Idade: {idade} saldo: {saldo:.2f}")
+print(f"Nome: {nome} Idade: {idade} saldo: {saldo:10.2f}")
