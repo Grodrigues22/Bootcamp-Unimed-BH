7620ea1fe18fb3ebcda24ed5e42426d03a88c9c5
Grodrigues22 <gmrodrigues98@gmail.com>
2022-11-03 18:56:25 -0300

    add_tuplas

---
+++ exemplos_das_aulas/06 - estrutura_de_dados/02 - tuplas/02 - acesso_direto.py
@@ -0,0 +1,11 @@
+# indices positivos
+
+frutas = ("maçã", "laranja", "uva", "pera",)
+frutas[0] # maçã
+frutas[2] # uva
+
+# indices negativos
+
+frutas[-1] # pera
+frutas[-3] # laranja
+
---
+++ exemplos_das_aulas/06 - estrutura_de_dados/02 - tuplas/04 - fatiamento.py
@@ -0,0 +1,7 @@
+tupla = ("p", "y", "t", "h", "o", "n",)
+
+print(tupla[2:]) # ("t", "h", "o", "n")
+print(tupla[:2]) # ("p", "y")
+print(tupla[1:3]) # ("y", "t")
+print(tupla[::]) # ("p", "y", "t", "h", "o", "n")
+print(tupla[::-1]) # ("n", "o", "h", "t", "y", "p")
