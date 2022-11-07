6c56bd8399c476003db1eb432afbb3e7213f4051
gmrodrigues98@gmail.com <gmrodrigues98@gmail.com>
2022-11-06 22:14:37 -0300

    rogerio

---
+++ exemplos_das_aulas/06 - estrutura_de_dados/04 - dicionarios_python/05_{}copy.py
@@ -0,0 +1,10 @@
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+   
+}
+
+copia = contatos.copy()
+copia["guilherme@gmail.com"] = {"nome": "Gui"}
+
+contatos["guilherme@gmail.com"] # {"nome": "Guilherme", "telefone": "3333-2221"}
+copia["guilherme@gmail.com"] #"nome": "Gui"
