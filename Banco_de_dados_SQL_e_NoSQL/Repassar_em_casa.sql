3e060d0787ca7bd845582097e37203287fc8d238
Grodrigues22 <gmrodrigues98@gmail.com>
2022-12-29 14:21:36 -0300

    29_12_22

--- Banco_de_dados_SQL_e_NoSQL/Repassar_em_casa.sql
+++ Banco_de_dados_SQL_e_NoSQL/Repassar_em_casa.sql
@@ -1,94 +1,52 @@
 -- apenas adiantando algumas atividades do curso, visto que no trabalho não tenho como baixar os programas necessarios 
 
--- Primeiros passos com SQL --> Persistindo informações no banco de dados Employee
+--> Explorando Queries com SQL
+        --> Manipulando o BD com comandos da DDL 
 
-        --> Realizado na tabela nova
+-- explorando os comandos ddl
 
-                -- Adicionando tabelas
-insert into dependent values (3334455555, 'Alice', 'F', '1986-04-05', 'Daughter'),
-                            (3334455555, 'Theodore', 'M', '1983-10-25', 'Son'),
-                            (3334455555, 'Joy', 'F', '1954-05-03', 'Spouse'),
-                            (987654321, 'Abner', 'M', '1942-02-28', 'Spouse'),
-                            (123456789, 'Michael', 'M', '1988-01-04', 'Son'),
-                            (123456789, 'Alice', 'F', '1988-12-30', 'Daughter'),
-                            (123456789, 'Elizabeth', 'F', '1967-05-05', 'Spouse');
+select now () as Timestamp;
 
-insert into departament values ('Research', 5, 3334455555, '1988-05-22', '1986-05-22'),
-                                ('Administration', 4, 987654321, '1995-01-01', '1995-01-01'),
-                                ('Headquarters', 1, 888665555, '1981-06-19', '1980-06-19');
+select database maniulation;
+create database if not exists manipulation;
+use manipulation;
 
-insert into dept_locations values (1, 'Houston'),
-                                (4, 'Stafford'),
-                                (5, 'Bellaire'),
-                                (5, 'Sugarland'),
-                                (5, 'Houston');
-
-insert into project values ('ProductX', 1, 'Bellaire', 5),
-                        ('ProductY', 2, 'Sugarland', 5),
-                        ('ProductZ', 3, 'Houston', 5),
-                        ('Computerization', 10, 'Stafford', 4),
-                        ('Reorganization', 20, 'Houston', 1),
-                        ('Newbenefits', 30, 'Stafford', 4);
-
-insert into work_on values (123456789, 1, 32.5),
-                        (123456789, 2, 7.5),
-                        (666884444, 3, 40.0),
-                        (453453453, 1, 20.0),
-                        (453453453, 2, 20.0),
-                        (333445555, 2, 10.0),
-                        (333445555, 3, 10.0),
-                        (333445555, 10, 10.0),
-                        (333445555, 20, 10.0),
-                        (999887777, 30, 30.0),
-                        (999887777, 10, 10.0),
-                        (987987987, 10, 35.0),
-                        (987987987, 30, 5.0),
-                        (987654321, 30, 20.0),
-                        (987654321, 20, 15.0),
-                        (888665555, 20, 0.0);
-
-select * from employee;
-
--- gerente e seu empregado
-select Ssn, Fname, Dname from employee e, departament d where (e.Ssn = d.Mgr_ssn);
-
--- recuperando dependentes dos empregados
-select Fname, Dependent_name, Relationship from employee, dependent where Essn = Ssn;
-
---
-select Bdate, Address from employee
-        where Fname = 'john' and Minit='B' and Lname='Smith';
-
--- Recuperando departamento específico
-select * from departament where Dname = 'Research';
-
-select Fname, Lname, Address from employee, departament
-        where Dname='Research' and Dnumber=Dno;
-
-desc work_on;
-select * from project;
-
-select Pname, Essn, Fname, Hours
-from project, work_on, employee
-        where Pnumber = Pno and Essn = Ssn;
-
-
-
-
-
-
-
-        --> Realizado na tabela antiga
-        
-                -- Retirado a lista referente a idade e colocado a linha "drop"
-drop table dependent;
-create table dependent(
-        Essn char(9) not null,
-        Dependent_name varchar(15) not null,
-        Sex char,
-        Bdate date,
-        Relationship varchar(8),
-        primary key (Essn, Dependent_name),
-        constraint fk_dependent foreign key (Essn) references employee(Ssn)
+CREATE TABLE bankAccounts (
+        id_account INT auto_increment PRIMARY KEY,
+        Ag_num INT NOT NULL,
+        Ac_num INT NOT NULL,
+        Saldo FLOAT,
+        CONSTRAINT identification_account_constraint UNIQUE (Ag_num, Ac_num)
 );
 
+alter table bankAccounts add LimiteCredito float 
+        not null default 500.00;
+alter table bankAccounts add email VARCHAR(60);
+alter table bankAccounts drop email;
+
+desc bankAccounts;
+
+CREATE TABLE bankClient(
+        id_cliente INT auto_increment,
+        ClientAccont INT,
+        CPF VARCHAR(11) NOT NULL,
+        RG VARCHAR(9) NOT NULL,
+        Nome VARCHAR(50) NOT NULL,
+        Endereço VARCHAR(100) NOT NULL,
+        Renda_mensal FLOAT,
+        primary key (id_cliente, ClientAccont),
+        CONSTRAINT fk_account_client foreign key (ClientAccont) REFERENCES bankAccounts(id_account)
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE bankTransactions(
+        id_transcation INT auto_increment PRIMARY KEY,
+        Ocorrencia datetime,
+        Status_transaction VARCHAR(20),
+        Source_account INT,
+        Destination_account INT,
+        CONSTRAINT fk_source_transaction foreign key (Source_account) REFERENCES
+        bankAccounts(id_account),
+        CONSTRAINT fk_destination_transaction foreign key (Destination_account) REFERENCES
+        bankAccounts(id_account)
+);
